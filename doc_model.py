70184b78ab0a0b26e0153ebb812d6010ba8698c0
Abdessattar <ghabi.1602@gmail.com>
2024-05-21 22:02:42 +0000

    Update

--- doc_model.py
+++ doc_model.py
@@ -8,17 +8,17 @@ Base = declarative_base()
 class doctor(Base):
     __tablename__ = "DOCTOR"
 
-    username = Column("username", String(128), nullable=False)
-    password = Column("pwd", String(128), nullable=False)
-    fullname = Column("fname", String(128), nullable=False)
-    email = Column("email", String(128), nullable=False)
-    location = Column("location", String(128), nullable=False)
-    speciality = Column("speciality", String(128), nullable=False)
-    degree = Column("degree", String(128), nullable=False)
-    license = Column("license", String(128), nullable=False)
-    status = Column("status", String(10), nullable=False)
-    availability = Column("availability", String(128), nullable=False)
+    username = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    fullname = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=False)
+    location = Column(String(128), nullable=False)
+    speciality = Column(String(128), nullable=False)
+    degree = Column(String(128), nullable=False)
+    license = Column(String(128), nullable=False)
+    status = Column(String(10), nullable=False)
+    availability = Column(String(128), nullable=False)
 
-    def __init__(self, *args, **kwargs):
-        """initializes a user"""
-        super().__init__(*args, **kwargs)
+   # def __init__(self, *args, **kwargs):
+   #     """initializes a user"""
+   #     super().__init__(*args, **kwargs)
